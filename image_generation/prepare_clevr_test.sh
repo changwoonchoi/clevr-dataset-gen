e02688071a632d9343a2cb0c9ec43a4936aad3e6
ccw <zzzmaster@snu.ac.kr>
2021-07-24 16:35:21 +0900

    add scripts

---
+++ image_generation/prepare_clevr_test.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+blender --background --python clevr_mv_constraint.py -- --use_gpu 1 --height 800 --width 800 --rot_with_xyz --num_view 400 --output_image_dir ../train/images --output_scene_dir ../train/scenes --output_scene_file ../train/CLEVR_scenes.json --transform_output_file ../train/transforms.json --save_blendfiles 1 --output_blend_dir ../train 
